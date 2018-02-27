67ad2f904fe2281dc730ec3294e1fdbc3e95342d
Piyathep <pmg_1037@hotmail.com>
2018-02-27 11:03:56 +0700

    Back Controller

--- app/src/main/java/piyathep/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
+++ app/src/main/java/piyathep/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package piyathep.krirk.ac.th.nearfinalexamination.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import piyathep.krirk.ac.th.nearfinalexamination.MainActivity;
 import piyathep.krirk.ac.th.nearfinalexamination.R;
 
 /**
@@ -20,6 +22,16 @@ public class CalculateFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbalCalculate);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
 
 //        Exchange Controller
 
