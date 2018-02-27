131c6b9ff7dc6b67758a78d0dd5a65f0b363c4d8
Piyathep <pmg_1037@hotmail.com>
2018-02-27 10:47:50 +0700

    Replace

---
+++ app/src/main/java/piyathep/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
@@ -0,0 +1,34 @@
+package piyathep.krirk.ac.th.nearfinalexamination.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import piyathep.krirk.ac.th.nearfinalexamination.R;
+
+/**
+ * Created by Piyathep on 27/2/2561.
+ */
+
+public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+//        Exchange Controller
+
+    }   // Main Method
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calculate, container, false);
+        return view;
+    }
+}
